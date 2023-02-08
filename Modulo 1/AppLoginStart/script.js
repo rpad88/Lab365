2fc5712c41409b7592f685c58860cdaf408b4f7c
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-02-08 19:16:28 -0400

    finalizado o exercício [m1s02] Ex2

--- Modulo 1/AppLoginStart/script.js
+++ Modulo 1/AppLoginStart/script.js
@@ -1,20 +1,43 @@
-const user = {
+const credentials = {
     name: 'estudante',
     password: '2023'
 }
 
-
-
-const uName = document.getElementById('uName')
-const pwd = document.querySelector('#pwd')
-
-const handleUserNameChange = (e) => {
-    user.name = event.target.value
+const user = {
+    name: '',
+    password: ''
 }
 
-const handlePasswordChange = (e) => {
-    console.log(e.target.value)
+// Selectors
+const userName = document.querySelector('[data-name]')
+const pwd = document.querySelector('[data-password]')
+const form = document.querySelector('#form')
+
+userName.addEventListener('change', (e) => {
+    e.preventDefault()
+    user.name = e.target.value
+})
+pwd.addEventListener('change', (e) => {
+    e.preventDefault()
+    user.password = e.target.value
+})
+
+function formValidation(login, password) {
+    if (login == credentials.name && password == credentials.password) return console.info('logado')
+
+    return console.warn('credenciais incorretas')
 }
 
-uName.addEventListener("change", handleUserNameChange)
-pwd.addEventListener('change', handlePasswordChange)
+function clear() {
+    userName.value = ''
+    pwd.value = ''
+    console.log('* inputs limpos');
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(`Username: ${user.name} 
+    Password: ${user.password}`)
+    formValidation(user.name, user.password)
+    clear()
+})
