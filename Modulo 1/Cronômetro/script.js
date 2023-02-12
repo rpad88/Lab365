c1b276bd532b003e081d7d8c9693c0ab3d94de9e
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-02-11 22:20:45 -0400

    Crônometro finalizado

--- Modulo 1/Cronômetro/script.js
+++ Modulo 1/Cronômetro/script.js
@@ -1,2 +1,15 @@
 const counter = document.querySelector('[data-counter]')
+const restartBtn = document.querySelector('[data-restart]')
+var initialTime = 10
 
+function timer() {
+    if(initialTime > 0) {
+        counter.textContent = initialTime
+        initialTime--
+    }
+    if(initialTime == 0) return counter.textContent = 'Seu tempo acabou!! Tente novamente!!'
+}
+
+restartBtn.addEventListener('click', () => initialTime = 10)
+
+setInterval(timer, 1000)
