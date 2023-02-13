7e3ac88bb19656bbe2b973c87e0b0e6ecf164a01
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-02-12 20:01:46 -0400

    comentários melhorados

--- Modulo 1/tabuada/tabuada.js
+++ Modulo 1/tabuada/tabuada.js
@@ -4,18 +4,19 @@ const ul = document.querySelector('[data-ul]')
 function multiplica(multiplicador) {
     let i = 1
     while(i <= 10) {
-        let li = document.createElement('li')
-        li.innerText = `${multiplicador} * ${i} = ${multiplicador * i}`
-        console.log(`${multiplicador} * ${i} = ${multiplicador * i}`)
-        ul.appendChild(li)
+        let li = document.createElement('li') //cria o elemento <li>
+        li.innerText = `${multiplicador} * ${i} = ${multiplicador * i}` //insere o texto: <li> conteúdo </li>
+        ul.appendChild(li) // insere o <li> criado como filho da <ul>
         i++
     }
 }
 
 document.forms.form.addEventListener('submit', (e, multiplicador = number.value) => {
     e.preventDefault()
+    // remove todos os <li> dentro do <ul>, caso tenha.
     while (ul.hasChildNodes()) {
         ul.removeChild(ul.firstElementChild)
     }
+    // chama a função multiplica passando como parâmetro o number.value (informado no input)
     multiplica(multiplicador)
 })
