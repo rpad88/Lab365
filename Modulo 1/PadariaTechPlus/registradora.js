c455ea1c14f65d216f04817107830d575ff2c7d7
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-02-15 22:10:59 -0400

    fixed totalCompra and added comments

--- Modulo 1/PadariaTechPlus/registradora.js
+++ Modulo 1/PadariaTechPlus/registradora.js
@@ -4,9 +4,11 @@ const vlrTotal = document.querySelector('[data-total-compra]')
 const vlrRecebido = document.querySelector('[data-money]')
 const vlrTroco = document.querySelector('[data-troco]')
 
+// contador de itens
 var contador = 1
 
 const App = {
+    // insere os <li> na tela.
     render(valor) {        
         let li = document.createElement('li')
         li.innerText = `item ${contador}: R$ ${valor}`
@@ -14,8 +16,9 @@ const App = {
         contador++
     },
     totalCompra() {
-        carrinhoDeCompras.reduce((acc, index) => {
-            total = acc + index
+        console.log('totalCompra chamado');
+        let total = carrinhoDeCompras.reduce((acc, index) => {
+           return acc + index
         })
         vlrTotal.textContent = `Total da Compra: R$ ${total}`
         return total.toFixed(2)
@@ -24,7 +27,7 @@ const App = {
         console.log(money)
         let troco = money - this.totalCompra()
         console.log(troco)
-        vlrTroco.textContent = `Valor do troco R$ ${troco.toFixed(2)} `
+        vlrTroco.textContent = `Valor do troco: R$ ${troco.toFixed(2)} `
     }
 }
 
@@ -32,12 +35,17 @@ var qtd = 1
 var valor
 
 do {
-    valor = Number(prompt(`Informe o valor do produto ${qtd}`))
+    valor = Number(prompt(`Informe o valor do produto ${qtd}`).replace(',','.'))
     carrinhoDeCompras.push(valor)
     App.render(valor)
     qtd++
 } while (valor !== 0) {
-    carrinhoDeCompras.pop()
-    let money = Number(prompt('Informe o valor recebido'))
+    carrinhoDeCompras.pop() //remove o último item do array que no caso seria 0, influenciando na quantidade de itens
+    App.totalCompra() //chama o cálculo da compra
+
+    // Informa o valor recebido
+    let money = Number(prompt('Informe o valor recebido').replace(',','.'))
+    vlrRecebido.textContent = `Valor recebido: R$ ${money}`
+    // passa para a função troco o valor recebido para o cálculo do troco.
     App.troco(money)
 }
