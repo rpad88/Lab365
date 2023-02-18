47fbf174f078658fef0a917a34cf2e4010ec9c48
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-02-18 11:43:21 -0400

    finalizada a pesquisa de

---
+++ Modulo 1/BuscadorDeUsuario/buscador.js
@@ -0,0 +1,19 @@
+import { users } from "./users.js"
+
+document.forms.find.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // console.log(users[3])
+
+    let userToFind = document.forms.find.usernameToFind.value
+    console.log(`Pesquisando por: ${userToFind}`);
+    let indexFound = users.findIndex(users => users.username == userToFind)
+    
+    if(indexFound >= 0) {
+        console.log(`Username encontrado no index ${indexFound}.`)
+
+        console.log(users[indexFound])
+        // console.log(JSON.stringify(users[indexFound]))
+    } else {
+        console.log('Username não encontrado.');
+    }
+})
