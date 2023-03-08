5630d8b9be96efef2db95363fbffe8d104bd8bf2
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-03-07 20:54:55 -0400

    iniciando servidor express

---
+++ Modulo 1/Node/Semana-6/backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+// app = {
+//     use,
+//     listen: recebe(porta, função callback),
+//     route
+// }
+
+app.listen(3333, () => {
+    console.log('servidor online')
+})
