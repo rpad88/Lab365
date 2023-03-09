d12a9af917501270da4de1aed9bb081fa7a3e5ee
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-03-09 11:10:13 -0400

    iniciando os exercícios da semana 6

--- Modulo 1/Node/Semana-6/backend/index.js
+++ Modulo 1/Node/Semana-6/backend/index.js
@@ -1,11 +1,34 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 // app = {
 //     use,
 //     listen: recebe(porta, função callback),
 //     route
 // }
+
+const devs = [
+    {name: "Fulano", email: "fulano@email.com"},
+    {name: "Beltrano", email: "beltrano@gmail.com"}
+]
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo')
+})
+app.get('/devs', (req, res) => {
+   return res.json(devs)
+})
+app.post('/rota/:nome', (req, res) => {
+    const {nome} = req.params
+    if(!nome) {
+        return res
+            .status(400)
+            .json({"erro": "Campo nome é obrigatório"})
+    }
+    res.send(`Rota de API criada pelo(a): ${nome}`)
+})
 
 app.listen(3333, () => {
     console.log('servidor online na porta 3333')
